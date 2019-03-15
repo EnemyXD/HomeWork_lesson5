e0b95b64e80867c161298a126f669dfe2ba32da6
enemy <nkalmykov937@gmail.com>
2019-03-15 17:03:42 +0300

    HomeWork

---
+++ HomeWork/HomeWork/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    class Employee
+    {
+        string _name;
+        string _lastname;
+        int _age;
+
+        public string Name;
+        public string LastName;
+        public int Age;
+
+        public override string ToString()
+        {
+            string empl = $"{Name} {LastName} {Age}";
+
+            return empl;
+        }
+    }
+}
